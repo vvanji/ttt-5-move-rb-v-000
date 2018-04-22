6c12e7cd0e7b870de56559e0f080386ceb5d8b30
Evangeline VIllena <vvanjie@yahoo.com>
2018-04-22 22:15:44 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(user_input)
+def input_to_index(input)
   converted_input = user_input.to_i - 1
 end
 
